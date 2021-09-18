dc9a447014195f3e0ab6c6f86aa40db498cf7a08
a-gambhir <agambhir99@gmail.com>
2021-09-18 17:43:30 -0400

    music functionality and package updates

---
+++ commands/play.js
@@ -0,0 +1,127 @@
+const ytdl = require('ytdl-core');
+const ytSearch = require('yt-search');
+
+
+
+const queue = new Map();
+
+module.exports = {
+    commands: ['play','skip', 'stop'],
+    expectedArgs:'<URL>',
+
+    
+    callback: async (message, args) => {
+
+        const voice_channel = message.member.voice.channel;
+
+        if(!voice_channel) return message.channel.send('You need to be in a voice channel to use this command!');
+        
+        const permissions = voice_channel.permissionsFor(message.client.user);
+        if(!permissions.has('CONNECT')) return message.channel.send('You do not have the correct permissions.');
+        if(!permissions.has('SPEAK')) return message.channel.send('You do not have the correct permissions.');
+
+        const server_queue = queue.get(message.guild.id);
+
+
+        if(message.content.includes('play')){
+            console.log("play")
+            if(!args.length) return message.channel.send('You need to provide a URL!');
+            let song = {}
+
+            if(ytdl.validateURL(args[0])){
+                const song_info = await ytdl.getInfo(args[0]);
+                song = { title: song_info.videoDetails.title, url: song_info.videoDetails.video_url}
+            } else{
+                //If video is not a URL then use keywords to find video
+                const video_finder = async(query) =>{
+                    const video_result = await ytSearch(query);
+                    return (video_result.videos.length > 1) ? video_result.videos[0] : null;
+                }
+
+                const video = await video_finder(args.join(' '));
+                if(video){
+                    song = { title: video.title, url: video.url}
+                } else {
+                    message.channel.send('There was an error finding the video.');
+                }
+            }
+
+
+            if (!server_queue){
+
+                const queue_constructor = {
+                    voice_channel: voice_channel,
+                    text_channel: message.channel,
+                    connection: null,
+                    songs: []
+                }
+
+                queue.set(message.guild.id, queue_constructor);
+                queue_constructor.songs.push(song)
+
+                try {
+                    const connection = await voice_channel.join();
+                    queue_constructor.connection = connection;
+                    video_player(message.guild, queue_constructor.songs[0]);
+                } catch(err){
+                    queue.delete(message.guild.id);
+                    message.channel.send('There was an error connecting!');
+                    throw err;
+                }
+
+            }else{
+                server_queue.songs.push(song);
+
+                //console.log(server_queue)
+
+                return message.channel.send(`***${song.title}*** has been added to the queue!`);
+            }
+        }
+
+        else if(message.content.includes('skip')) skip_song(message, server_queue);
+        else if(message.content.includes('stop')) stop_song(message, server_queue);
+          
+    }
+
+
+
+}
+
+
+
+        //function to play the audio from the video
+        const video_player = async (guild, song) => {
+            const song_queue = queue.get(guild.id);
+        
+            if(!song){
+                console.log("no more songs")
+                song_queue.voice_channel.leave();
+                queue.delete(guild.id);
+                return;
+            }
+            const stream = ytdl(song.url, {filter: 'audioonly' });
+            song_queue.connection.play(stream, { seek: 0, volume: 0.5 }) 
+            .on('finish', () => {
+               
+                song_queue.songs.shift();
+                video_player(guild, song_queue.songs[0]);
+            });
+            await song_queue.text_channel.send(`Now playing **${song.title}**`);
+        }
+
+        const skip_song = (message, server_queue) => {
+            if(!message.member.voice.channel) return message.channel.send('You need to be in a voice channel to use this command!');
+            console.log("skip")
+            if(!server_queue){
+                return message.channel.send('There are no songs in the queue.');
+            }
+            server_queue.connection.dispatcher.end();
+        }
+
+        const stop_song = (message, server_queue) => {
+            if(!message.member.voice.channel) return message.channel.send('You need to be in a voice channel to use this command!');
+            console.log("stop")
+            server_queue.songs = [];
+            server_queue.connection.dispatcher.end();
+
+        }
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const fs = require('fs');//fs = file system module
 const path = require('path');
 const Discord = require('discord.js'); 
 const{ RichEmbed } = require('discord.js');
+const ytdl = require("ytdl-core");
 const ms = require('ms'); // ms package converts time to ms
 const { prefix, token, ownerID } = require('./config.json');
 const client = new Discord.Client();
@@ -11,48 +12,7 @@ const cooldowns = new Discord.Collection();
 
 
 
-//searching directory for commands
-/*
-function search(dir){
-  const commandList = new Discord.Collection();
-  files = fs.readdirSync(dir, {'withFileTypes': true});
 
-  for(const file of files){
-    const name = file.name;
-    if(file.isDirectory()){
-
-      console.log(name);  
-      commandList[name] = search(`${dir}/${name}`); //if directory, search again until a file is found
-      
-  
-    }
-    else if (name.endsWith('.js')){
-      const command = require(`${dir}/${name}`);//require looks at file for module and exports it
-      commandList.set(command.name, command);
-      console.log(command.name);
-
-    }
-
-  }
-  return commandList;
-}
-
-const client = new Discord.Client();
-client.commands = new Discord.Collection(); 
-
-
- client.commands = search('./commands');
-
-
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('js')); //returns array of all JS filenames in "commands"folder
-
-for(const file of commandFiles){
-  const command = require(`./commands/${file}`);
-  
-  client.commands.set(command.name, command);
-}
-
-*/
 
 
 client.on('ready', async () => { //when client is ready, print to console
@@ -86,74 +46,7 @@ client.on('ready', async () => { //when client is ready, print to console
   readCommands('commands')
 })
 
-/*
-client.on('message', message => {
 
- 
-  if(!message.content.startsWith(prefix) || message.author.bot) return;//if message doesnt start w prefix,ignore it
-
-  const args = message.content.slice(prefix.length).split(/ +/);
-  const commandName = args.shift().toLowerCase();
-
-  console.log(commandName);
-
-
-  let command = client.commands.get(commandName)
-    || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
-
-   if(!command){
-     command = client.commands.moderation.get(commandName);
-   } 
-
-
-  if(command.guildOnly && message.channel.type != 'text'){
-    return message.reply('I can\'t execute that command inside DMs.');
-  }
-
-  if (command.args && !args.length){
-   let reply = `You didn't provide any arguments.`;
-  
-
-  if(command.usage){
-    reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
-  }
-
-  return message.channel.send(reply);
-}
-
-  if(!cooldowns.has(command.name)) {
-    cooldowns.set(command.name, new Discord.Collection());
-  }
-
-  const now = Date.now(); //current timestamp
-  const timestamps = cooldowns.get(command.name);//command to look for cooldown of
-  const cooldownAmount = (command.cooldown || 3) * 1000; //default cooldown time 3 sec- but must convert to mili
-
-  if(timestamps.has(message.author.id)){  //check if timestamps collection has authorID in it yet
-    const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
- 
-  if(now < expirationTime) {
-    const timeLeft =(expirationTIme = now) / 1000;
-    return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
-    } 
-  }
-  
-  timestamps.set(message.author.id, now);
-  setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
-
-
-
-  try {
-    command.execute(message, args);
-  } catch(error){
-    console.error(error);
-
-    message.reply('there was an error trying to execute that command!');
-  }
-
-  
-});
-*/
 
 
 //Deleting message audit log- bot needs VIEW_AUDIT_LOGS permission
@@ -328,3 +221,8 @@ client.on('messageReactionRemove', (messageReaction, user) => {
   }
 
 })
+
+
+
+
+client.login(token);
